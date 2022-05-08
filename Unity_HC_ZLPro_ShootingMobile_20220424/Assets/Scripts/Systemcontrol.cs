cda6dba69e1b651e7012d3057276009797c20609
Reduichi <57396815+Reduichi@users.noreply.github.com>
2022-05-08 12:58:52 +0800

    123

--- Unity_HC_ZLPro_ShootingMobile_20220424/Assets/Scripts/Systemcontrol.cs
+++ Unity_HC_ZLPro_ShootingMobile_20220424/Assets/Scripts/Systemcontrol.cs
@@ -13,6 +13,12 @@ namespace RED
         private Joystick joystick;
         [SerializeField, Header("移動速度"), Range(0, 300)]
         private float speed = 10.5f;
+        [SerializeField, Header("角色方向圖示")]
+        private Transform traDirectionicon;
+        [SerializeField, Header("角色方向圖示範圍"), Range(0, 5)]
+        private float rangeDirectionicon = 2.5f;
+        [SerializeField, Header("角色旋轉速度"), Range(0, 100)]
+        private float speedturn = 1.5f;
 
         private Rigidbody rig;
 
@@ -24,7 +30,10 @@ namespace RED
 		private void Update()
 		{
             // GetJoystickValue();
-		}
+            UpdateDirectionIconPos();
+            LookDirectionIcon();
+
+        }
 
 		private void FixedUpdate()
 		{
@@ -46,6 +55,30 @@ namespace RED
         {
             // 剛體.加速度 = 三維向量(x，y，z)
             rig.velocity = new Vector3(-joystick.Horizontal, 0, -joystick.Vertical) * speed;
+        }
+
+        /// <summary>
+        /// 變更角色圖示的座標
+        /// </summary>
+        private void UpdateDirectionIconPos()
+        {
+            // 新座標 = 角色的座標 + 三維向量(虛擬搖桿的水平與垂直) * 方向圖示的範圍
+            Vector3 pos = transform.position + new Vector3(-joystick.Horizontal, 0.5f, -joystick.Vertical) * rangeDirectionicon;
+            // 更新方向圖示的座標 = 新座標
+            traDirectionicon.position = pos;
+        }
+
+        /// <summary>
+        /// 面向方向圖示
+        /// </summary>
+        private void LookDirectionIcon()
+        {
+            // 取得面向角度 = 四位元.面向角度(方向圖示 - 角色) - 方向圖示與角色的向量
+            Quaternion look = Quaternion.LookRotation(traDirectionicon.position - transform.position);
+            // 角色的角度 = 四位元.插值(角色的角度，面向角度，旋轉速度 * 一偵的時間)
+            transform.rotation = Quaternion.Lerp(transform.rotation, look, speedturn * Time.deltaTime);
+            // 角色的歐拉角度 = 三維向量(0，原本的歐拉角度Y，0)
+            transform.eulerAngles = new Vector3(0, transform.eulerAngles.y, 0);
         }
     }
 
